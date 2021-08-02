29c2ac6fc802a48c4553469ec8ce17e9e97b1b56
Christopher Iliyas <christopheriliyas@gmail.com>
2021-08-02 03:03:49 +0100

    add received box

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -10,6 +10,17 @@ export class CommentComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.setTextAreaHeight()
+  }
+
+  setTextAreaHeight(): void {
+    const textArea = document.getElementById('post-comment');
+
+    textArea.setAttribute('style', 'height:' + (textArea.scrollHeight) + 'px;overflow-y:hidden;');
+    textArea.addEventListener('input', () => {
+      textArea.style.height = 'auto';
+      textArea.style.height = (textArea.scrollHeight) + 'px';
+    }, false);
   }
 
 }
